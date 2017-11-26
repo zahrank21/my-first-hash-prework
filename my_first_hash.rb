95e98a9791d14e59dbd90d36412d8fc811834335
zahrank21 <zahrank@gmail.com>
2017-11-26 12:30:43 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ end
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding

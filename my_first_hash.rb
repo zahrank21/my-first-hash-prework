55054bfe7471bcd2d8ec3fd46b340de1cdaa351d
zahrank21 <zahrank@gmail.com>
2017-11-26 12:29:22 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  new_hash = {"steven" => 1, "bob" => 2}
   
 end
 

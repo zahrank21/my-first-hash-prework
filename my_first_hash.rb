9cc96f1cef8c4768640773d1ced2bf5035c53302
zahrank21 <zahrank@gmail.com>
2017-11-26 12:31:54 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,5 +21,6 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
 
 end
